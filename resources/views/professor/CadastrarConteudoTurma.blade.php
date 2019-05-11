5d9d18f0d395ec62acb0b9a594de2b589ecedd38
Alana <alanatenorioelias@gmail.com>
2019-05-10 21:22:31 -0300

    inserida funcionalidade de adicionar conteúdos e suas dependências

--- resources/views/professor/CadastrarConteudoTurma.blade.php
+++ resources/views/professor/CadastrarConteudoTurma.blade.php
@@ -1,6 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
+<script language='javascript'>
+
+var conteudos = {!! json_encode($conteudos) !!};
+
+function adicionar(){
+  var listaDependencias = document.getElementById("dependencias");
+  var idx = listaDependencias.selectedIndex;
+	var which = listaDependencias.options[idx].value;
+
+  var conteudosSelecionados = document.getElementById("dependenciasSelecionadas");
+
+  for (var i=0; i<conteudos.length; i++) {
+		if(conteudos[i].id == which) {
+      newOption = document.createElement("option");
+      newOption.value = conteudos[i].id;
+      newOption.text=conteudos[i].nome;
+      conteudosSelecionados.add(newOption);
+    }
+  }
+
+  listaDependencias.remove(idx);
+
+}
+
+function remover(){
+  var dependenciasSelecionadas = document.getElementById("dependenciasSelecionadas");
+  var idx = dependenciasSelecionadas.selectedIndex;
+	var which = dependenciasSelecionadas.options[idx].value;
+
+  var listaDependencias = document.getElementById("dependencias");
+
+  for (var i=0; i<conteudos.length; i++) {
+		if(conteudos[i].id == which) {
+      newOption = document.createElement("option");
+      newOption.value = conteudos[i].id;
+      newOption.text = conteudos[i].nome;
+      listaDependencias.add(newOption);
+    }
+  }
+
+  dependenciasSelecionadas.remove(idx);
+}
+
+function selectAll() {
+        selectBox = document.getElementById("dependenciasSelecionadas");
+
+        for (var i = 0; i < selectBox.options.length; i++)
+        {
+             selectBox.options[i].selected = true;
+        }
+}
+
+</script>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -8,10 +61,17 @@
                 <div class="card-header">{{ __('Inserir Conteúdo') }}</div>
 
                 <div class="card-body">
+                  @if (\Session::has('success'))
+                  <br>
+                      <div class="alert alert-success">
+                          {!! \Session::get('success') !!}
+                      </div>
+                  @endif
                     <form method="POST" action="{{ route('/conteudo/cadastrar') }}">
                       {{ csrf_field() }}
                         @csrf
                         <input type="hidden" name="turma_id" value="{{ $turma->id}}" />
+
 
                         <div class="form-group row">
                             <label for="nome" class="col-md-4 col-form-label text-md-right">{{ __('Nome ') }}</label>
@@ -27,13 +87,57 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="descricao" class="col-md-4 col-form-label text-md-right">{{ __('Descricao ') }}</label>
+
+                            <div class="col-md-6">
+                              <textarea name="descricao" id="descricao" type="text" class="form-control"> {{ $errors->first('descricao')}}</textarea>
+
+                                @error('descricao')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+
+                            <div class="container">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <label>Dependências</label>
+                                  <select id="dependencias" class="form-control" multiple>
+                                    @foreach ($conteudos as $conteudo)
+                                      <option value="{{$conteudo->id}}">{{ $conteudo->nome }}</option>
+                                    @endforeach
+
+                                  </select>
+                                </div>
+
+                                <div class="col-md-6">
+                                  <label>Dependências Selecionadas</label>
+                                  <select id="dependenciasSelecionadas" name="dependenciasSelecionadas[]" class="form-control" multiple>
+                                  </select>
+                                </div>
+                              </div>
+                              <br>
+                              <a class="btn btn-primary" onClick="adicionar();">+</a>
+                              <a class="btn btn-primary" onClick="remover();">-</a>
+
+                            </div>
+
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                              <button type="submit" class="btn btn-primary">
-                                  Inserir
+                              <button type="submit" class="btn btn-primary" onClick="selectAll();">
+                                  Cadastrar
                               </button>
                             </div>
                         </div>
+
                     </form>
                 </div>
             </div>
--- resources/views/professor/EditarConteudo.blade.php
+++ resources/views/professor/EditarConteudo.blade.php
@@ -1,6 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
+
+<script language='javascript'>
+
+var conteudos = {!! json_encode($conteudos) !!};
+
+function adicionar(){
+  var listaDependencias = document.getElementById("dependencias");
+  var idx = listaDependencias.selectedIndex;
+	var which = listaDependencias.options[idx].value;
+
+  var conteudosSelecionados = document.getElementById("dependenciasSelecionadas");
+
+  for (var i=0; i<conteudos.length; i++) {
+		if(conteudos[i].id == which) {
+      newOption = document.createElement("option");
+      newOption.value = conteudos[i].id;
+      newOption.text=conteudos[i].nome;
+      conteudosSelecionados.add(newOption);
+    }
+  }
+
+  listaDependencias.remove(idx);
+
+}
+
+function remover(){
+  var dependenciasSelecionadas = document.getElementById("dependenciasSelecionadas");
+  var idx = dependenciasSelecionadas.selectedIndex;
+	var which = dependenciasSelecionadas.options[idx].value;
+
+  var listaDependencias = document.getElementById("dependencias");
+
+  for (var i=0; i<conteudos.length; i++) {
+		if(conteudos[i].id == which) {
+      newOption = document.createElement("option");
+      newOption.value = conteudos[i].id;
+      newOption.text = conteudos[i].nome;
+      listaDependencias.add(newOption);
+    }
+  }
+
+  dependenciasSelecionadas.remove(idx);
+}
+
+function selectAll() {
+        selectBox = document.getElementById("dependenciasSelecionadas");
+
+        for (var i = 0; i < selectBox.options.length; i++)
+        {
+             selectBox.options[i].selected = true;
+        }
+}
+
+</script>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -28,9 +83,56 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="descricao" class="col-md-4 col-form-label text-md-right">{{ __('Descricao') }}</label>
+
+                            <div class="col-md-6">
+                              <input name="descricao" id="descricao" type="text" class="form-control" value="{{ $conteudo->descricao}}"  > {{ $errors->first('descricao')}}
+
+                                @error('descricao')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+
+                            <div class="container">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <label>Dependências</label>
+                                  <select id="dependencias" class="form-control" multiple>
+                                    @foreach ($conteudos as $dependencia)
+                                    @if($dependencia->id != $conteudo->id)
+                                      <option value="{{$dependencia->id}}">{{ $dependencia->nome }}</option>
+                                    @endif
+                                    @endforeach
+
+                                  </select>
+                                </div>
+
+                                <div class="col-md-6">
+                                  <label>Dependências Selecionadas</label>
+                                  <select id="dependenciasSelecionadas" name="dependenciasSelecionadas[]" class="form-control" multiple>
+                                    @foreach ($dependencias as $dependencia)
+                                      <option value="{{$dependencia->id}}">{{ $dependencia->nome }}</option>
+                                    @endforeach
+                                  </select>
+                                </div>
+                              </div>
+                              <br>
+                              <a class="btn btn-primary" onClick="adicionar();">+</a>
+                              <a class="btn btn-primary" onClick="remover();">-</a>
+
+                            </div>
+
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                              <button type="submit" class="btn btn-primary">
+                              <button type="submit" class="btn btn-primary" onClick="selectAll();">
                                   Salvar
                               </button>
                             </div>
