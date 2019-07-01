c72d607b20d7ed3933ef4515f6811fea8d50eb04
Alana <alanatenorioelias@gmail.com>
2019-07-01 20:55:42 -0300

    atualizações de rotas

--- resources/views/professor/ListarAtividades.blade.php
+++ resources/views/professor/ListarAtividades.blade.php
@@ -6,7 +6,10 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Atividades') }}</div>
+                <div class="card-header">{{ __('Atividades') }}
+                <a href="{{ route("home") }}">Início</a> >
+                <a href="{{ route("/turma/gerenciar", ["id" => $turma->id]) }}">{{$turma->nome}}</a> >
+                Atividades cadastradas</div>
 
                 <div class="card-body">
 
